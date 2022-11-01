fe0d38d13707b751b57afeb5bb03cd8925a3fadc
Pugialli <joaopugialli@gmail.com>
2022-11-01 19:09:53 -0300

    aula01

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme'
+import { SignIn } from './src/screens/Singin';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        fontsLoaded ? <SignIn /> : <Loading />
+      }
+    </NativeBaseProvider>
+  );
+}
